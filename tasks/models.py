7d76ef611ec824e382e05fc34ba1ef4d88a78e72
Abdulmukhit <asalachik@gmail.com>
2025-03-11 02:19:47 +0500

    added User, Title, Description models

--- tasks/models.py
+++ tasks/models.py
@@ -1,4 +1,26 @@
 from django.db import models
 
-class TaskManager(models.Model):
+
+class User(models.Model):
+ IMPORTANCE_OF_TASKS = [
+     ("E", "easy"),
+     ("N", "normal"),
+     ("D", "difficult"),
+     ]
+ name = models.CharField(max_length=100)
+ importance = models.CharField(choices=IMPORTANCE_OF_TASKS, default="E", max_length=1)
+
+
+
+
+
+class Title(models.Model):
     task_name = models.CharField(max_length=100)
+
+class Description(models.Model):
+    task_description = models.CharField(max_length=100)
+
+def main():
+ pass
+if __name__ == "__main__":
+ main()
